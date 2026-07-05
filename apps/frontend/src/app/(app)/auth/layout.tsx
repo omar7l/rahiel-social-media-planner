281f210138a39d236899561844b67807aa292c95
Omar Rahiel <omar.rahiel7@gmail.com>
2026-07-05 03:18:05 +0200

    Restore dark auth form colors

--- apps/frontend/src/app/(app)/auth/layout.tsx
+++ apps/frontend/src/app/(app)/auth/layout.tsx
@@ -15,7 +15,7 @@ export default async function AuthLayout({
   const t = await getT();
 
   return (
-    <div className="bg-[#0E0E0E] flex flex-1 p-[12px] gap-[12px] min-h-screen w-screen text-white">
+    <div className="dark bg-[#0E0E0E] flex flex-1 p-[12px] gap-[12px] min-h-screen w-screen text-white">
       {/*<style>{`html, body {overflow-x: hidden;}`}</style>*/}
       <ReturnUrlComponent />
       <div className="flex flex-col py-[40px] px-[20px] flex-1 lg:w-[600px] lg:flex-none rounded-[12px] text-white p-[12px] bg-[#1A1919]">
